b55a11786bf76b8693e26255117d94e3652c7d21
Prajwal <prajwaln22@gmail.com>
2016-03-23 13:30:42 +0530

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,4 +1,6 @@
-class admin {
-$tittle=prajwal
-users::managed_users{$tittle:
+class users::admin {
+users::managed_users{"kelly":}
+users::managed_users{"kriss":}
+users::managed_users{"harry":
+  group => admin,
 }
