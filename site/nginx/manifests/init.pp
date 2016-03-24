5d5ec5d0ae8ecafc35f2f14d3804861d093ba844
Prajwal <prajwaln22@gmail.com>
2016-03-24 13:15:48 +0530

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,62 @@
-class nginx inherits nginx::params{
-File {
-  owner => 'root',
-  group => 'root',
-  mode => '0664',
- } 
- package { 'nginx':
-   ensure => present,
+class nginx (
+  $confdir      = $nginx::params::confdir,
+  $logdir       = $nginx::params::logdir,
+  $nginx_user   = $nginx::params::nginx_user,
+  $package_name = $nginx::params::package_name,
+  $file_owner   = $nginx::params::file_owner,
+  $file_group   = $nginx::params::file_group,
+  $docroot      = $nginx::params::docroot,
+) inherits nginx::params {
+  
+  File {
+    owner => $file_owner,
+    group => $file_group,
+    mode => '0644',
   }
-  file { '/var/www':
+  
+  # package nginx
+  package {'nginx': 
+    ensure => present,
+    name   => $package_name,
+  }
+  
+  # document root /var/www
+  file {$docroot:
     ensure => directory,
   }
-  file {'/var/www/index.html':
+  
+  # index.html
+  file {"${docroot}/index.html":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-  file {'/etc/nginx/nginx.conf':
+  
+  file {"${confdir}/conf.d":
+    ensure => directory,
+  }
+  
+  # config file nginx.conf
+  file {"${confdir}/nginx.conf":
     ensure => file,
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
+    #source => 'puppet:///modules/nginx/nginx.conf',
+    content => template('nginx/nginx.conf.erb'),
     notify => Service['nginx'],
+    require => Package['nginx'],
   }
-  file {'/etc/nginx/conf.d':
-    ensure => 'directory',
+  
+  # config default.conf
+  file {"${confdir}/conf.d/default.conf":
+    ensure => file,
+    #source => 'puppet:///modules/nginx/default.conf',
+    content => template('nginx/default.conf.erb'),
+    notify => Service['nginx'],
+    require => Package['nginx'],
   }
- file {'/etc/nginx/conf.d/default.conf':
-   ensure => file,
-   source => 'puppet:///modules/nginx/default.conf',
-   require => Package['nginx'],
-   notify => Service['nginx'],
-   }
-   
-  service { 'nginx':
+  
+  # service
+  service {'nginx':
     ensure => running,
     enable => true,
-  }  
+  }
 }
-  
-   
+
