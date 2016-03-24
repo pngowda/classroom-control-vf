ad0f4c4f1b0e241b036d9f2fd8ae1f7e4f964748
Prajwal <prajwaln22@gmail.com>
2016-03-24 13:16:09 +0530

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,3 +1,37 @@
 class nginx::params {
-  $index_html_source='puppet:///modules/nginx/index.html'
+
+  case $::osfamily {
+    'RedHat' : {
+      $confdir      = '/etc/nginx'
+      $logdir       = '/var/log/nginx'
+      $nginx_user   = 'nginx'
+      $package_name = 'nginx'
+      $file_owner   = 'root'
+      $file_group   = 'root'
+      $docroot      = '/var/www'
+    }
+    'debian' : {
+      $confdir      = '/etc/nginx'
+      $logdir       = '/var/log/nginx'
+      $nginx_user   = 'www-data'
+      $package_name = 'nginx'
+      $file_owner   = 'root'
+      $file_group   = 'root'
+      $docroot      = '/var/www'
+    }
+    'windows' : {
+      $confdir      = 'C:/ProgramData/nginx/html'
+      $logdir       = 'C:/ProgramData/nginx/logs'
+      $nginx_user   = 'nobody'
+      $package_name = 'nginx-service'
+      $file_owner   = 'Administrator'
+      $file_group   = 'Administrators'
+      $docroot      = 'C:/ProgramData/nginx/html'
+    }
+    default : {
+      fail("nginx module does not support operating system ${::osfamily}")
+    }
+  }
+
 }
+
